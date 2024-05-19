3669b219d9c7ac57fd64150254f5edd37e134a4a
davidmbrust <112821939+davidmbrust@users.noreply.github.com>
2024-05-19 15:58:25 -0500

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -2,17 +2,22 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-            base: process.env.NODE_ENV === 'production' ? '/jungbaejusite' : '',
-		}
-	}
+  kit: {
+    adapter: adapter({
+      fallback: '404.html'
+    }),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/jungbaejusite' : ''
+    },
+    prerender: {
+      handleHttpError: ({ path, referrer, message }) => {
+        // Log the error and continue
+        console.warn('Prerender error', { path, referrer, message });
+        // You can choose to suppress the error by returning an empty object
+        return { error: null };
+      }
+    }
+  }
 };
 
 export default config;
